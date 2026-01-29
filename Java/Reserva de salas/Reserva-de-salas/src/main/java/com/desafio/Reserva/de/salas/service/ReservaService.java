0b44a84c62de2fead4ebb33f75cd427942782ab6
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-01-29 18:26:30 -0300

    Update ReservaService.java

--- Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/service/ReservaService.java
+++ Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/service/ReservaService.java
@@ -1,13 +1,40 @@
 package com.desafio.Reserva.de.salas.service;
 
 import com.desafio.Reserva.de.salas.model.Reserva;
+import com.desafio.Reserva.de.salas.repository.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservaService {
-    public void salvarReserva(Reserva reserva) {
-        if (reserva.getHoraInicio().isAfter(reserva.getHoraFim())) {
-            throw new IllegalArgumentException("A hora de início deve ser antes da hora de fim.");
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    // Método para salvar uma reserva
+    public void CriarReserva(Reserva reserva) {
+        if (reserva == null) {
+            throw new IllegalArgumentException("A reserva não pode ser nula");
+        }
+        if (reserva.getDataHoraInicio() == null || reserva.getDataHoraFim() == null) {
+            throw new IllegalArgumentException("O horário de início e fim da reserva não podem ser nulos");
+        }
+        if (reserva.getDataHoraFim().isBefore(reserva.getDataHoraInicio())) {
+            throw new IllegalArgumentException("O horário de fim da reserva não pode ser antes do horário de início");
+        }
+        if (reservaRepository.existsById((long) reserva.getId())) {
+            throw new IllegalArgumentException("Já existe uma reserva com este ID");
+        } else {
+            reservaRepository.save(reserva);
+        }
+    }
+
+    // Metodo para deletar uma reserva por ID
+    public void DeletarReservaPorId(Long id) {
+        if (!reservaRepository.existsById(id)) {
+            throw new IllegalArgumentException("Reserva não encontrada com o ID: " + id);
+        } else {
+            reservaRepository.deleteById(id);
         }
     }
 }
