8a169cbfdd955a63d6ef71bf6989032873f4acc8
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-02-01 10:01:01 -0300

    Update Usuario.java

--- Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/model/Usuario.java
+++ Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/model/Usuario.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long usuarioId;
 
     private String nome;
 }
