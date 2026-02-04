62f44714e2ca3f99d2c7c81e4ab843fa79cca41c
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-02-04 19:02:07 -0300

    Create SalaService.java

---
+++ Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/business/service/SalaService.java
@@ -0,0 +1,27 @@
+package com.desafio.Reserva.de.salas.business.service;
+
+import com.desafio.Reserva.de.salas.infrastructure.model.Sala;
+import com.desafio.Reserva.de.salas.infrastructure.repository.SalaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalaService {
+
+    @Autowired
+    private SalaRepository salaRepository;
+
+    public List<Sala> listarSalas() {
+        return salaRepository.findAll();
+    }
+
+    public void criarSala(Sala sala) {
+        try {
+            salaRepository.save(sala);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Erro ao criar sala: " + e.getMessage());
+        }
+    }
+}
