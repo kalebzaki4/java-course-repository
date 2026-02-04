9079d2d78ff638b7dce06a2da41938033e4920f1
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-02-04 19:02:05 -0300

    Create ReservaService.java

---
+++ Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/business/service/ReservaService.java
@@ -0,0 +1,57 @@
+package com.desafio.Reserva.de.salas.business.service;
+
+import com.desafio.Reserva.de.salas.infrastructure.exception.EntidadeNaoEncontradaException;
+import com.desafio.Reserva.de.salas.infrastructure.model.Reserva;
+import com.desafio.Reserva.de.salas.infrastructure.model.Sala;
+import com.desafio.Reserva.de.salas.infrastructure.repository.ReservaRepository;
+import com.desafio.Reserva.de.salas.infrastructure.repository.SalaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReservaService {
+
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    @Autowired
+    private SalaRepository salaRepository;
+
+    // criar uma reserva
+    public void CriarReserva(Reserva reserva) {
+        if (reserva.getSala() == null || reserva.getSala().getId() == null) {
+            throw new IllegalArgumentException("O ID da sala deve ser informado.");
+        }
+
+        Sala salaExistente = salaRepository.findById(reserva.getSala().getId()).orElseThrow(() -> new IllegalArgumentException("A sala especificada não existe"));
+
+        boolean conflito = reservaRepository.existsBySalaIdAndDataReservaAndHoraInicioBeforeAndHoraFimAfter(salaExistente.getId(), reserva.getDataReserva(), reserva.getHoraFim(), reserva.getHoraInicio());
+
+        if (conflito) {
+            throw new IllegalArgumentException("Conflito de horário: A sala já está reservada nesse período.");
+        }
+
+        reserva.setSala(salaExistente);
+        reservaRepository.save(reserva);
+    }
+
+    // deletar uma reserva por ID
+    public void DeletarReservaPorId(Long id) {
+        if (!reservaRepository.existsById(id)) {
+            throw new EntidadeNaoEncontradaException("Reserva com ID " + id + " não encontrada.");
+        } else {
+            reservaRepository.deleteById(id);
+            System.out.println("Reserva com ID " + id + " deletada com sucesso.");
+        }
+    }
+
+    public List<Reserva> ListarTodasReservas() {
+        try {
+            return reservaRepository.findAll();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Erro ao listar reservas: " + e.getMessage());
+        }
+    }
+}
