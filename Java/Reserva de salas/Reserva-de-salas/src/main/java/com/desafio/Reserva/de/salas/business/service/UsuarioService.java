4cbf96b55630e47f4f1d69332fea7d6b19f41a9e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-02-05 20:48:14 -0300

    Update UsuarioService.java

--- Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/business/service/UsuarioService.java
+++ Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/business/service/UsuarioService.java
@@ -1,12 +1,11 @@
 package com.desafio.Reserva.de.salas.business.service;
 
-import com.desafio.Reserva.de.salas.controller.UsuarioController;
 import com.desafio.Reserva.de.salas.infrastructure.model.Usuario;
 import com.desafio.Reserva.de.salas.infrastructure.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -14,14 +13,15 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public List<Usuario> ListarUsuarios() {
+    public Page<Usuario> listarUsuarios(Pageable pageable) {
         try {
-            return usuarioRepository.findAll();
+            return usuarioRepository.findAll(pageable);
         } catch (Exception e) {
             throw new IllegalArgumentException("Erro ao listar usuarios: " + e.getMessage());
         }
     }
 
+    // Metodo para criar usuario
     public void criarUsuario() {
         try {
             Usuario usuario = new Usuario();
@@ -30,5 +30,22 @@ public class UsuarioService {
         } catch (Exception e) {
             throw new IllegalArgumentException("Erro ao criar usuario: " + e.getMessage());
         }
+    }
+
+    // metodo para deletar usuario
+    public void deletarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+        System.out.println("Usuario com id " + id + " deletado com sucesso.");
+    }
+
+    // metodo para atualizar usuario
+    public void atualizarUsuario(Long id, Usuario usuarioAtualizado) {
+        Usuario usuarioExistente = usuarioRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Usuario com id " + id + " não encontrado."));
+
+        usuarioExistente.setNome(usuarioAtualizado.getNome());
+
+        usuarioRepository.save(usuarioExistente);
+        System.out.println("Usuario com id " + id + " atualizado com sucesso.");
     }
 }
