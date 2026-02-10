deb66ad7360c1a2384981b20dba0550559e38235
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2026-02-10 20:24:55 -0300

    adicionando testes com Mocks pro LimpezaReservaTask

--- Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/business/service/LimpezaReservaTask.java
+++ Java/Reserva de salas/Reserva-de-salas/src/main/java/com/desafio/Reserva/de/salas/business/service/LimpezaReservaTask.java
@@ -4,21 +4,38 @@ import com.desafio.Reserva.de.salas.infrastructure.repository.ReservaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Component
 public class LimpezaReservaTask {
     @Autowired
-    private ReservaRepository reservaRepository;
+    ReservaRepository reservaRepository;
 
-    @Scheduled(fixedRate = 3600000)
+    private Clock clock = Clock.systemDefaultZone();
+
+    @Autowired
+    public Clock getClock() {
+        return this.clock;
+    }
+
+    @Autowired
+    public void setClock(Clock fixed) {
+        this.clock = fixed;
+    }
+
+    @Transactional
+    @Scheduled(fixedRate = 300000)
     public void excluirReservasVencidas() {
-        LocalDate hoje = LocalDate.now();
-        LocalTime agora = LocalTime.now();
-        reservaRepository.deleteByDataReservaBeforeOrDataReservaAndHoraFimBefore(hoje, hoje, agora);
+        LocalDate hoje = LocalDate.now(clock);
+        LocalTime agora = LocalTime.now(clock);
+        LocalDate dataLimite = hoje.minusDays(1);
+        reservaRepository.deleteByDataReservaBeforeOrDataReservaAndHoraFimBefore(dataLimite, hoje, agora);
 
         System.out.println("🧹 Faxina realizada: Reservas antigas removidas.");
     }
+
 }
